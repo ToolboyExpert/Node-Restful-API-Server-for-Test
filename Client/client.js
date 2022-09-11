117eda6a4ed3fbe2978356a2a76975cb99f1d9ec
Tool Boy Expert <dnjswns119@naver.com>
2022-09-11 22:29:38 +0900

    Second commit

---
+++ Client/client.js
@@ -0,0 +1,4 @@
+const root_call = 'curl localhost:3000/';
+const user_list_call = 'curl localhost:3000/users';
+const add_user_call = 'curl -X PUT localhost:3000/users';
+const delete_user_call = 'curl -X DELETE localhost:3000/users';
